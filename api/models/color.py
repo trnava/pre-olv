e4bac9ec0b5cf8c5ea1107f7d4f9b9acbc741090
Zett-8 <kame0008@gmail.com>
2018-12-18 20:42:16 +0900

    add work-seed

--- api/models/color.py
+++ api/models/color.py
@@ -4,3 +4,6 @@ from django.db import models
 class Color(models.Model):
     name = models.CharField(max_length=10)
     color_code = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
